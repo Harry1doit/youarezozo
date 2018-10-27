b7bd45172b49abf0df7cc6344342878601c5944d
harry <m.tsubasa0@gmail.com>
2018-10-27 06:49:16 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # 保存の成功をここで扱う。
+       log_in @user
        flash[:success] = "Welcome to the You are ZOZO!"
        redirect_to @user
     else
