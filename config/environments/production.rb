e050ea4ac03d0cfb533e04467f2fb1ee34e5d672
harry <m.tsubasa0@gmail.com>
2018-10-31 01:09:59 +0000

    added s3 to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   
   config.active_storage.service = :amazon
-
+  config.require_master_key = true
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
