08b97dbc8e4a54880c5524c84b4076c4849cd924
harry <m.tsubasa0@gmail.com>
2018-10-31 10:24:03 +0000

    Add user following

--- config/initializers/carrieawave.rb
+++ config/initializers/carrieawave.rb
@@ -1,13 +1,14 @@
-if Rails.env == 'production'
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-config.fog_credentials = {
- provider: 'AWS',
- aws_access_key_id: ENV['ACCESS_KEY_ID'],
- aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
- region: 'ap-northeast-1'
- }
-config.fog_directory = 'my-app-first-bucket'
- config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/my-app-first-bucket'
-end
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['AKIAI4N5ZK2D2B5G7ETA'],  # required
+    aws_secret_access_key: ENV['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'],     # required
+    region:                'ap-northeast-1',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
+    #host:                  's3.example.com',             # optional, defaults to nil
+    #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'my-app-first-bucket'                # required
+  config.fog_public     = false                                                 # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
