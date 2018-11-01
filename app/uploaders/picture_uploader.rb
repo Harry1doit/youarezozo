ff9f36bc67e0c19a6b34745ad80a82d118cd8e42
harry <m.tsubasa0@gmail.com>
2018-11-01 00:32:35 +0000

    Add

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,14 +8,15 @@ class PictureUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  # アップロードファイルの保存先ディレクトリは上書き可能
-  # 下記はデフォルトの保存先  
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # アップロード可能な拡張子のリスト
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -14,5 +15,6 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
   end
 end
