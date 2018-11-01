9c3656c3c016e473ae1eb9f9871dde9873cc7b5d
harry <m.tsubasa0@gmail.com>
2018-11-01 12:36:18 +0000

    Delete

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,9 +1,5 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-<<<<<<< HEAD
-    config.fog_provider = 'fog/aws'
-=======
->>>>>>> following-users
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
