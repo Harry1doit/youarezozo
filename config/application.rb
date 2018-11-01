8b89406ce3fb6ba2ad44f2dab21d740838a63794
harry <m.tsubasa0@gmail.com>
2018-11-01 03:25:59 +0000

    Add user microposts

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.initialize_on_precompile = false
+
 
 module SampleApp
   class Application < Rails::Application
@@ -15,6 +17,5 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile = false
   end
 end
