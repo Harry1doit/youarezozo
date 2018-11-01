71e22343f243ee438d0426ed41fd8631a889f679
harry <m.tsubasa0@gmail.com>
2018-11-01 03:15:19 +0000

    Add user microposts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'railstutorial-yasulab.c9users.io'
+  host = 'cadfe15a9c8d405ba42cc09fc0714310.vfs.cloud9.ap-southeast-1.amazonaws.com'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['AKIAI4N5ZK2D2B5G7ETA'],
+      :aws_secret_access_key => ENV['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']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  ENV['my-app-first-bucket']
   end
 end
