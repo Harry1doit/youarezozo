f9a07f9bee2eb9ff9a73c04d10ab1fbde529478f
harry <m.tsubasa0@gmail.com>
2018-11-02 00:01:50 +0000

    New

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,6 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module SampleApp
-  config.action_view.embed_authenticity_token_in_remote_forms = true
-  config.assets.initialize_on_precompile = false
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
